ca2916c67929608adaa414684e84daca96a32345
Kelvin <kelvin.martinez92@gmail.com>
2019-11-19 14:34:01 -0500

    first updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+
+var guess = [
+    
+    document.onkeyup = function(event) {
+
+        
+    var userInput = event.key.toLowerCase();
+
+    //this is for when the user inputs the incorrect answer//
+    if (userInput === "1" || userInput === "2" || userInput === "3" || userInput === "5") {
+
+     alert("incorrect!");
+
+    }
+
+  }
+
+
+
+
+]
+
+    
+
+  
+
+
