d164749246ab5e6722f43b27e07cec2e342dcf37
Kelvin <kelvin.martinez92@gmail.com>
2019-11-21 18:37:57 -0500

    added variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,23 @@
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    var wins = 0;
+    var loses = 0; 
+    var tries = 0;
 
-var guess = [
-    
+
+    for(i=0, i)
     document.onkeyup = function(event) {
 
-        
-    var userInput = event.key.toLowerCase();
 
-    //this is for when the user inputs the incorrect answer//
-    if (userInput === "1" || userInput === "2" || userInput === "3" || userInput === "5") {
+   
+    if (userInput === letters) {
+
+
 
      alert("incorrect!");
 
     }
 
-  }
+
 
 
 
