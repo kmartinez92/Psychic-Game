0b8e2b17e7693c25c91bff189bb040641dd5589f
Kelvin <kelvin.martinez92@gmail.com>
2019-11-22 15:21:03 -0500

    updated javascript with for loop for number guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,27 @@
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    var wins = 0;
-    var loses = 0; 
-    var tries = 0;
-    var correctguess = "a";
 
 
-    for(i=0, letters.length)
-    document.onkeyup = function(event) {
+var guess = prompt("Enter Guess Below");
+var answer = 4;
+var wins = document.getElementById("correct")
 
+alert.prompt("Guess a number between 1-10");
 
-   
-    if (userInput === correctguess) {
-
-        alert("You won!")
-        document.getElementById("#wins1")
-
-        
-
-
-
-     alert("incorrect!");
+for(i=0; i<0; i++){
+    if(answer == guess){
+        alert("Good Job!")
+        wins++;
 
     }
 
 
 
 
+}
 
 
-]
 
+
+    
     
 
   
